efb155d4c3d3cd3e1541f273cd5b076db480dd00
Bogdan <bogdan.mihai@aau.at>
2021-12-22 15:29:12 +0100

    100FL Rounds

--- FedAdapt/results/see_results.py
+++ FedAdapt/results/see_results.py
@@ -2,3 +2,8 @@ import pickle
 with open('FedAdapt_res.pkl', 'rb') as f:
     data = pickle.load(f)
 print(data)
+
+with open('Client_time_metrics.pkl', 'rb') as f2:
+    data2 = pickle.load(f2)
+
+print(data2)
